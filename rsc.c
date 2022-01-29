ffe8f6d821bb55d086d8502184a3489ebfa169d4
Sunerniu <1814361313@qq.com>
2022-01-29 23:53:15 +0800

    K

--- rsc.c
+++ rsc.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_LINE 1024
+#define SZ 100010
 
 long long int ip1(char *oip1){
     int l;
-    int a[4]={0};
+    int a[SZ]={0};
     int f=0;
     int flag=0;
-    int p[32];
+    int p[SZ]={0};
     long long int H=0;
     long long int M=1;
-    
+
 for(int i=0;oip1[i]!='\0';i++){
     if(flag==0){
     a[f]=oip1[i]-'0';
@@ -27,91 +27,60 @@ for(int i=0;oip1[i]!='\0';i++){
 
 }
 
-for(l=0;a[3]!=0;l++){
+for(l=31;l>=24;l--){
     if(a[3]%2==1)
     p[l]=1;
     else p[l]=0;
     a[3]=a[3]/2;
 }
-for(l;a[2]!=0;l++){
+for(l=23;l>=16;l--){
     if(a[2]%2==1)
     p[l]=1;
     else p[l]=0;
     a[2]=a[2]/2;
 }
-for(l;a[1]!=0;l++){
+for(l=15;l>=8;l--){
     if(a[1]%2==1)
     p[l]=1;
     else p[l]=0;
     a[1]=a[1]/2;
 }
-for(l;a[0]!=0;l++){
+for(l=7;l>=0;l--){
     if(a[0]%2==1)
     p[l]=1;
     else p[l]=0;
     a[0]=a[0]/2;
 }
 
-for(int m=0;m<l;m++){
 
+for(int m=31;m>=0;m--){
    if(p[m]==1){ 
      
     H+=M;
 
 }  
-
-M=M*2;
-
-    
+M=M*2;  
 }
+
 return H;
 }   
 //输入IP地址转出十进制
-
-void packet(char *pp){
-   char packet[1024]; 
-    long long int *O;
-   FILE *fp;            
-   int len;             
-    fp = fopen(pp,"r");
-    while(fgets(packet,1024,fp) != NULL) {
-     long long int pk1,pk2;
-     int pk3,pk4,pk5;
-     sscanf(packet,"%lld %lld %d %d %d",&pk1,&pk2,&pk3,&pk4,&pk5);
-    O[0]==pk1;
-    O[1]==pk2;
-    O[2]==pk3;
-    O[3]==pk4;
-    O[4]==pk5;
-    printf("%d",R(O));
- 
- 
- 
- 
- 
- 
- 
- 
- 
- } 
-}
-
-
-int R(char *P)
-{int li=0;
- char rule[MAX_LINE];  
- FILE *fp;            
+long long int R(long long int *P)
+{long long int li=-1;
+ char rule[SZ];  
+ FILE *fp;        
  int len;             
 fp = fopen("rule1.txt","r");
- while(fgets(rule,MAX_LINE,fp) != NULL)
- {  li++;
-     int u=0;
+ while(fgets(rule,SZ,fp) != NULL)
+ {  ++li;
+    int u=0;
     int i=1;
     int flag=0;
-    char rule1[20]={0};
-    char rule2[20]={0}; 
-    char dk[10]={0};
-    int ra1=0,ra2=0,ra3=0,ra4=0;   
+    char rule1[SZ]={0};
+    char rule2[SZ]={0}; 
+    char dk[SZ]={0};
+    int ra1=0,ra2=0,ra3=0,ra4=0;
+    
     for(i;rule[i]!='/';i++)
     {
         rule1[i-1]=rule[i];
@@ -161,25 +130,27 @@ ra4=ra4*10+rule[i]-'0';
     for(int I=0;I<9;i++,I++){
         dk[I]=rule[i];
     }
-
-
 long long int u1=ip1(rule1);
 long long int u2=ip1(rule2);
-if(P[0]==u1){
-    if(P[1]==u2)
-    {
-        if(P[2]>=ra1&&P[3]<=ra2){
-        if(P[3]>=ra3&&P[4]<=ra3){
-                    if(dk[8]=='0')
-                    {
-                        break;
-                    }
-                    else {
-                        if((dk[2]-'0')*16+dk[3]-'0'==P[4]
-                                break;
 
+long long int y=(dk[2]-'0')*16+dk[3]-'0';
+
+if(P[0]==u1){
+    if(P[1]==u2){
+        if(P[2]>=ra1&&P[2]<=ra2){
+            if(P[3]>=ra3&&P[3]<=ra4){
+                if(dk[8]=='0'){
+                    
+                    return li;
+                     
+                }
+                else {
+                    if(y==P[4]){
+                            
+                      return li;
                     }
-        }
+                }
+            }
         }
     }
 }
@@ -189,5 +160,39 @@ if(P[0]==u1){
 
 
  }
-  return li;
+
+ return -1;
+}
+
+void packet(char *pp){
+   char packet[SZ]; 
+    long long int O[SZ];
+   FILE *fp;
+   FILE *ii=fopen("io","w");          
+   int len;
+   int a=1;             
+    fp = fopen(pp,"r"); 
+    while(fgets(packet,SZ,fp) != NULL) {
+     long long int pk1,pk2;
+     int pk3,pk4,pk5;
+     sscanf(packet,"%lld %lld %d %d %d",&pk1,&pk2,&pk3,&pk4,&pk5);
+    O[0]=pk1;
+    O[1]=pk2;
+    O[2]=pk3;
+    O[3]=pk4;
+    O[4]=pk5;
+    long long int K=R(O);
+    
+    printf("%lld\n",K);
+ } 
+}
+
+
+
+
+int main(){  
+    
+    packet("packet1.txt");
+    
+    return 0;
 }
